98452d09d8e319e42e8702195711293e8978f4ea
RajababuShah <rajababushah.in@gmail.com>
2022-12-24 17:19:32 +0545

    v1.1

---
+++ index.js
@@ -0,0 +1,29 @@
+console.log(
+    "Javascript Calculator Made by Rajababu Shah \nhttps://r-rajababushah.github.io"
+);
+
+document.getElementById('answer').readOnly = true;
+
+let screen = document.getElementById('answer');
+let buttons = document.querySelectorAll('button');
+
+let screenValue = "";
+
+for (item of buttons) {
+    item.addEventListener('click', (e) => {
+        let buttonText = e.target.innerHTML;
+        if (buttonText == "X") {
+            buttonText = "*";
+            screenValue = screenValue + buttonText;
+            screen.value = screenValue;
+        } else if (buttonText == "C") {
+            screenValue = "";
+            screen.value = screenValue;
+        } else if (buttonText == "=") {
+            screen.value = eval(screenValue);
+        } else {
+            screenValue = screenValue + buttonText;
+            screen.value = screenValue;
+        }
+    });
+}
